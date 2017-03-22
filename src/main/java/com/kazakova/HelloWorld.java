992a444314b2fab94694b3092f2d2786c6ed5ac3
Казакова Наталья Викторовна <SBT-Kazakova-NV@sigma.sbrf.ru>
2017-03-22 19:16:15 +0400

    first commit

---
+++ src/main/java/com/kazakova/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kazakova;
+
+/**
+ * Created by sbt-kazakova-nv on 22.03.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Helfcdxlo!!!");
+    }
+}
